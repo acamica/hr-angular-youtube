4d87f9196cb52b9ab4bd07adc811b664eed723d8
Hernan Rajchert <hrajchert@gmail.com>
2017-01-19 16:29:56 -0300

    fix(player-model): Add missing types that should later on be PR into definitly typed

---
+++ src/service/youtube-api.types.ts
@@ -0,0 +1,11 @@
+namespace YT {
+    export type VideoQuality = 'highres' | 'hd1080' | 'hd720' | 'large' | 'medium' | 'small';
+    export type SuggestedVideoQuality = 'default';
+
+    // tslint:disable-next-line: interface-name
+    export interface Player {
+        getPlaybackQuality(): VideoQuality;
+        setPlaybackQuality(suggestedQuality: SuggestedVideoQuality): void;
+        getAvailableQualityLevels(): VideoQuality[];
+    }
+}
