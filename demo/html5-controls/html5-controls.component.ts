288ae378c19cfd227213ee0d2a905f4e3852d260
Hernan Rajchert <hrajchert@gmail.com>
2017-01-23 10:58:42 -0300

    feat(demo-html5-controls): Added initial dummy version of html5 controls player

---
+++ demo/html5-controls/html5-controls.component.ts
@@ -0,0 +1,24 @@
+import {Component} from 'src/ng-helper/component';
+import 'src/main';
+import 'hrAngularYoutubeTpl';
+
+@Component({
+    selector: 'html5ControlsDemo',
+    templateUrl: '/demo/html5-controls/html5-controls.component.html'
+})
+class Html5ControlsDemoComponent {
+    private video: HTMLVideoElement;
+
+    static $inject = ['$element'];
+    constructor(private elm) {
+        this.video = this.elm.find('video')[0];
+    }
+
+    playVideo() {
+        this.video.play();
+    }
+    pauseVideo() {
+        this.video.pause();
+    }
+
+}
