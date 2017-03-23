cb209a4540a7570cd9b22ad0029b9274f6fd50ac
Hernan Rajchert <hrajchert@gmail.com>
2017-03-23 13:39:33 -0300

    fix(demo-overlay): Fix setting options

--- demo/overlay/overlay.component.ts
+++ demo/overlay/overlay.component.ts
@@ -1,6 +1,7 @@
 import * as angular from 'angular';
 import {Component} from 'rx-player/ng-helper/facade';
 import {RxPlayerComponent} from 'rx-player/directive/rx-player.component';
+import {setPlayerVarDefaultOption} from 'rx-player/players/youtube/youtube.service';
 import 'rx-player/main';
 import 'ui.bootstrap';
 import 'rx-player/ng-helper/async.filter';
@@ -8,17 +9,19 @@ import 'rx-player/ng-helper/async.filter';
 // TODO: Refactor to @Injectable and providers
 // http://blog.rangle.io/configurable-services-in-angular-2/
 // Create the app module and configure it
+// angular
+//     .module('demoOverlay', ['rxPlayer', 'rxPlayerTpls', 'ui.bootstrap'])
+//     .config(['youtubeProvider', configureYoutubeProvider]);
 angular
-    .module('demoOverlay', ['rxPlayer', 'rxPlayerTpls', 'ui.bootstrap'])
-    .config(['youtubeProvider', configureYoutubeProvider]);
+    .module('demoOverlay', ['rxPlayer', 'rxPlayerTpls']);
 
-function configureYoutubeProvider (youtubeProvider) {
-    // This options are the ones from here
-    // https://developers.google.com/youtube/player_parameters?playerVersion=HTML5
-    youtubeProvider.setPlayerVarOption('controls', 0);
-    youtubeProvider.setPlayerVarOption('rel', 0);
-    youtubeProvider.setPlayerVarOption('modestbranding', 1);
-}
+// function configureYoutubeProvider (youtubeProvider) {
+//     // This options are the ones from here
+//     // https://developers.google.com/youtube/player_parameters?playerVersion=HTML5
+setPlayerVarDefaultOption('controls', 0);
+setPlayerVarDefaultOption('rel', 0);
+setPlayerVarDefaultOption('modestbranding', 1);
+// }
 
 
 @Component({
