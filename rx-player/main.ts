8002515aad63b605c27ad295f6a61b6b755b9c70
Hernan Rajchert <hrajchert@gmail.com>
2017-04-06 17:37:59 -0300

    refactor(main): Modulirize exports by folder facades

--- rx-player/main.ts
+++ rx-player/main.ts
@@ -1,25 +1,7 @@
-// TODO: Dirty hack to load everything, refactor later
-export * from './directive/yt-slider.directive';
-export * from './overlay/hr-yt-marker.directive';
-export * from './overlay/player-current-quality.directive';
-export * from './overlay/player-current-speed.directive';
-export * from './overlay/player-current-time.directive';
-export * from './overlay/player-panel.component';
-export * from './overlay/progress-bar/player-progress-bar-hover-indicator.component';
-export * from './overlay/progress-bar/player-progress-bar.component';
-export * from './overlay/player-repeat-available-quality.directive';
-export * from './overlay/player-repeat-available-speed.directive';
-export * from './overlay/player-set-quality.directive';
-export * from './overlay/player-set-speed.directive';
-export * from './overlay/player-total-time.directive';
-export * from './overlay/player-volume-horizontal.component';
-// Reenable once the markers are refactored
+export * from './directive/yt-slider.directive'; // TODO: This folder shouldn't exists
+export * from './overlay/facade'; // TODO: split accordingly and rename to controls
 export * from './markers/facade';
-export * from './players/rx-player.component';
-export * from './players/youtube/youtube-player.model';
-export * from './players/video-player.model';
-export * from './players/youtube/youtube.service';
-import './players/html5/html5-player.service';
+export * from './players/facade';
 import './util/rx/rx-operators-import';
 
 
---
+++ rx-player/players/facade.ts
@@ -0,0 +1,5 @@
+export * from './rx-player.component';
+export * from './youtube/youtube-player.model';
+export * from './video-player.model';
+export * from './youtube/youtube.service';
+import './html5/html5-player.service';
