fc1b822bb5e3bb59f98d0a912e0297e92e5b5cd6
Hernan Rajchert <hrajchert@gmail.com>
2017-04-28 17:03:35 -0300

    feat(utils): Improve RxStore types

--- rx-player/util/store.util.ts
+++ rx-player/util/store.util.ts
@@ -1,10 +1,9 @@
 // import {Subject} from 'rxjs/Subject';
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/delay';
 
-export interface IReducer<S, A extends IAction> {
-    (state: S, action?: A): S;
-}
+export type IReducer<S, A extends IAction> = (state: S | undefined, action: A) => S;
 
 export interface IAction {
     type: string;
@@ -17,15 +16,6 @@ export class Store<S, A extends IAction> {
     private _dispacher: BehaviorSubject<S>;
 
     constructor (private reducer: IReducer<S, A>) {
-        // this._state = this._dispacher
-        //     // Starts with an initial action that shouldn't be mapped
-        //     .startWith({type: 'INITIAL_ACTION'})
-        //     .scan(reducer, undefined) // Pass undefined as accu/state so it can use the default
-        //     .publishReplay(1).refCount();
-        // this._state = Observable.of({type: 'INITIAL_ACTION'})
-            // Starts with an initial action that shouldn't be mapped
-            // .scan(reducer, undefined) // Pass undefined as accu/state so it can use the default
-            // .publish(1).refCount();
         const action = {type: '___INITIAL_ACTION___'} as A;
         this._dispacher = new BehaviorSubject(reducer(undefined, action));
         this._state = this._dispacher
@@ -38,7 +28,6 @@ export class Store<S, A extends IAction> {
     dispatch (action: A) {
         this._dispacher.next(this.reducer(this._dispacher.value, action));
     }
-
 
     select (): Observable<S>;
     select<K1 extends keyof S> (arg1: K1): Observable<S[K1]>;
@@ -75,3 +64,13 @@ export function combineReducers<ST, A extends IAction> (reducers: IReducerMap<ST
         );
     };
 }
+
+export interface IPayloadAction<A, P> {
+    type: A;
+    payload: P;
+}
+
+export interface ISimpleAction<A> {
+    type: A;
+}
+
